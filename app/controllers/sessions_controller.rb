6cfa0e92700188906af0004dd16030c8850eebd3
Branko Culum <brankoculum@gmail.com>
2018-04-28 12:25:37 -0400

    Trying to deploy to Heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome, you've loged in"
+      flash[:notice] = "Welcome, you've logged in"
       redirect_to root_path
     else
       flash.now[:error] = "There is something wrong with your username or password."
