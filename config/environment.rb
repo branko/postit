808bc7cb41c61e25022b2baf9d67bc2db02bad11
Branko Culum <brankoculum@users.noreply.github.com>
2018-08-24 13:45:55 -0400

    Fixed typo

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 PostitTemplate::Application.initialize!
-ENV['RAILS_ENV'] || = 'production'
+ENV['RAILS_ENV'] ||= 'production'
